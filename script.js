90d7f10cc84d4c971b611d62e789f278bae4194a
OwaissHassan <khabarquss@gmail.com>
2023-01-15 21:17:35 +0530

    fixed navbar

--- script.js
+++ script.js
@@ -1,48 +1,85 @@
-$(document).ready(function () {
-    $("#menu").click(function () {
-      $(this).toggleClass("fa-times");
-      $(".navbar").toggleClass("nav-toggle");
-    });
+// $(document).ready(function () {
+//     $("#menu").click(function () {
+//       $(this).toggleClass("fa-times");
+//       $(".navbar").toggleClass("nav-toggle");
+//     });
   
-    $(window).on("scroll load", function () {
-      $("#menu").removeClass("fa-times");
-      $(".navbar").removeClass("nav-toggle");
-    });
+//     $(window).on("scroll load", function () {
+//       $("#menu").removeClass("fa-times");
+//       $(".navbar").removeClass("nav-toggle");
+//     });
   
-    $(".portfolio .button-container .btn").click(function () {
-      let filter = $(this).attr("data-filter");
+//     $(".portfolio .button-container .btn").click(function () {
+//       let filter = $(this).attr("data-filter");
   
-      if (filter == "all") {
-        $(".portfolio .image-container .box").show("400");
-      } else {
-        $(".portfolio .image-container .box")
-          .not("." + filter)
-          .hide("200");
-        $(".portfolio .image-container .box")
-          .filter("." + filter)
-          .show("400");
-      }
-    });
+//       if (filter == "all") {
+//         $(".portfolio .image-container .box").show("400");
+//       } else {
+//         $(".portfolio .image-container .box")
+//           .not("." + filter)
+//           .hide("200");
+//         $(".portfolio .image-container .box")
+//           .filter("." + filter)
+//           .show("400");
+//       }
+//     });
   
-    $("#theme-toggler").click(function () {
-      $(this).toggleClass("fa-sun");
-      $("body").toggleClass("dark-theme");
-    });
+//     $("#theme-toggler").click(function () {
+//       $(this).toggleClass("fa-sun");
+//       $("body").toggleClass("dark-theme");
+//     });
   
-    // smooth scrolling
+//     // smooth scrolling
   
-    $('a[href*=""]').on("click", function (e) {
-      e.preventDefault();
+//     $('a[href*=""]').on("click", function (e) {
+//       e.preventDefault();
   
-      $("html, body").animate(
-        {
-          scrollTop: $($(this).attr("href")).offset().top,
-        },
-        500,
-        "linear"
-      );
-    });
+//       $("html, body").animate(
+//         {
+//           scrollTop: $($(this).attr("href")).offset().top,
+//         },
+//         500,
+//         "linear"
+//       );
+//     });
+//   });
+
+
+
+
+ /* new changes (15-jan-2023) */
+
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navItem = document.querySelectorAll(".nav__item"),
+  header = document.getElementById("header");
+
+// open and close menu
+navToggle.addEventListener("click", () => {
+  navMenu.classList.toggle("nav__menu--open");
+  changeIcon();
+});
+
+// close the menu when the user clicks the nav links
+navItem.forEach((item) => {
+  item.addEventListener("click", () => {
+    if (navMenu.classList.contains("nav__menu--open")) {
+      navMenu.classList.remove("nav__menu--open");
+    }
+    changeIcon();
   });
+});
+
+// Change nav toggle icon
+function changeIcon() {
+  if (navMenu.classList.contains("nav__menu--open")) {
+    navToggle.classList.replace("ri-menu-3-line", "ri-close-line");
+  } else {
+    navToggle.classList.replace("ri-close-line", "ri-menu-3-line");
+  }
+}
+
+
   
   //email
  
